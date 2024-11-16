dc5c2860cfa679202e89a18033ffe2a0b8075871
komasandesu <ryoweikomatani@gmail.com>
2024-11-17 03:18:16 +0900

    修正

--- app/routes/profile.$username.tsx
+++ app/routes/profile.$username.tsx
@@ -75,15 +75,15 @@ export default function UserProfile() {
 
             {/* プロフィール編集ボタン */}
             {user.id === profileUser.id && (
-              <div>
-                <Link to="/profile/settings" className="flex items-center text-blue-600 hover:underline dark:text-blue-400">
-                  {/* プロフィール編集アイコン */}
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                  </svg>
-                  プロフィールを編集する
-                </Link>
-              </div>
+              <Link
+                to={`/profile/${profileUser.name}/settings`}
+                className="inline-flex items-center px-4 py-2 border rounded bg-blue-500 text-white hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-700"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mr-2 w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+                プロフィール編集
+              </Link>
             )}
           </div>
 
