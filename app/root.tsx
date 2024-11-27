16a6e3bbbdbdfe12af2a58f66d81241b5e2b77d1
komasandesu <ryoweikomatani@gmail.com>
2024-11-27 23:57:48 +0900

    OGP修正

--- app/root.tsx
+++ app/root.tsx
@@ -36,10 +36,12 @@ export const meta: MetaFunction = () => {
       property: "og:title",
       content: "bubutter の butter の部分",
     },
-    {
-      name: "description",
-      content: "Share Partial Tweet",
-    },
+    { name: 'description', content: '部分ったー - 部分を投稿して共有するプラットフォームです。' },
+    { property: 'og:description', content: '部分を投稿して共有するプラットフォームです。' },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:site_name', content: 'bubutter' },
+    { property: 'og:url', content: 'https://bubutter.at-math.com/' },
+    { property: 'og:image', content: 'https://bubutter.at-math.com/og-image.png' },
   ];
 };
 
