e8fc74f291d78364bbc01383cf7ba74ef96eaac7
komasandesu <ryoweikomatani@gmail.com>
2024-12-13 19:45:37 +0900

    修正

--- app/root.tsx
+++ app/root.tsx
@@ -29,21 +29,21 @@ export const links: LinksFunction = () => [
   },
 ];
 
-// export const meta: MetaFunction = () => {
-//   return [
-//     { title: "bubutter" },
-//     {
-//       property: "og:title",
-//       content: "bubutter の butter の部分",
-//     },
-//     { name: 'description', content: '部分ったー - 部分を投稿して共有するプラットフォームです。' },
-//     { property: 'og:description', content: '部分を投稿して共有するプラットフォームです。' },
-//     { property: 'og:type', content: 'website' },
-//     { property: 'og:site_name', content: 'bubutter' },
-//     { property: 'og:url', content: 'https://bubutter.at-math.com/' },
-//     { property: 'og:image', content: 'https://bubutter.at-math.com/og-image.png' },
-//   ];
-// };
+export const meta: MetaFunction = () => {
+  return [
+    { title: "bubutter" },
+    {
+      property: "og:title",
+      content: "bubutter の butter の部分",
+    },
+    { name: 'description', content: '部分ったー - 部分を投稿して共有するプラットフォームです。' },
+    { property: 'og:description', content: '部分を投稿して共有するプラットフォームです。' },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:site_name', content: 'bubutter' },
+    { property: 'og:url', content: 'https://bubutter.at-math.com/' },
+    { property: 'og:image', content: 'https://bubutter.at-math.com/og-image.png' },
+  ];
+};
 
 
 export function Layout({ children }: { children: React.ReactNode }) {
